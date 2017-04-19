e9275cb4da126d9c8405120b234a4e6f3e6f3e60
shatunovda <shatunovda@gmail.com>
2017-04-20 00:37:19 +0400

    test

--- app/src/main/java/com/example/den/flagquiz/MainActivityFragment.java
+++ app/src/main/java/com/example/den/flagquiz/MainActivityFragment.java
@@ -1,11 +1,17 @@
 package com.example.den.flagquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -14,8 +20,12 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -79,6 +89,7 @@ public class MainActivityFragment extends Fragment {
         }
 
         questionNumberTextView.setText(getString(R.string.question, 1, FLAGS_IN_QUIZ));
+
         return view;
     }
 
@@ -93,9 +104,113 @@ public class MainActivityFragment extends Fragment {
         }
     }
 
-    public void updateRegion(SharedPreferences defaultSharedPreferences) {
+    public void updateRegion(SharedPreferences sharedPreferences) {
+        regionSet = sharedPreferences.getStringSet(MainActivity.REGIONS, null);
     }
 
     public void resetQuiz() {
+        AssetManager asset = getActivity().getAssets();
+        fileNameList.clear();
+        try {
+            for (String region : regionSet) {
+                String [] paths = asset.list(region);
+
+                for (String path : paths) {
+                    fileNameList.add(path.replace(".png", ""));
+                }
+            }
+        } catch (IOException ioe) {
+            Log.e(TAG, "Error loading image file names", ioe);
+        }
+
+        correctAnswers = 0;
+        totalGuesses = 0;
+        quizCountriesList.clear();
+
+        int flagCounter = 1;
+        int numberOfFlags = fileNameList.size();
+
+        while (flagCounter <= FLAGS_IN_QUIZ) {
+            int randomIndex = random.nextInt(numberOfFlags);
+            String fileName = fileNameList.get(randomIndex);
+            if (!quizCountriesList.contains(fileName)) {
+                quizCountriesList.add(fileName);
+                ++flagCounter;
+            }
+        }
+        loadNextFlag();
     }
+
+    private void loadNextFlag() {
+        String nextImage = quizCountriesList.remove(0);
+        correctAnswer = nextImage;
+        answerTextView.setText("");
+
+        questionNumberTextView.setText(getString(R.string.question, (correctAnswers + 1), FLAGS_IN_QUIZ ));
+
+        String region = nextImage.substring(0, nextImage.indexOf("-"));
+        AssetManager assets = getActivity().getAssets();
+        try (InputStream stream = assets.open(region + "/" + nextImage + ".png")) {
+            Drawable flag = Drawable.createFromStream(stream, nextImage);
+            flagImageView.setImageDrawable(flag);
+
+            animate(false);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Error loading " + nextImage, ioe);
+        }
+
+        Collections.shuffle(fileNameList);
+        int correct = fileNameList.indexOf(correctAnswer);
+        fileNameList.add(fileNameList.remove(correct));
+
+        for (int row = 0; row < guessRows; row++) {
+            for (int column = 0; column < guessLinearLayouts[row].getChildCount(); column++) {
+                Button newGuessButon = (Button) guessLinearLayouts[row].getChildAt(column);
+                newGuessButon.setEnabled(true);
+
+                String fileName = fileNameList.get((row * 2) + column);
+                newGuessButon.setText(getCountryName(fileName));
+            }
+        }
+
+        int row =random.nextInt(guessRows);
+        int column = random.nextInt(2);
+        LinearLayout randomRow = guessLinearLayouts[row];
+        String countryName = getCountryName(correctAnswer);
+        ((Button) randomRow.getChildAt(column)).setText(countryName);
+
+    }
+
+    private void animate(boolean animateOut) {
+        if (correctAnswers == 0) {
+            return;
+        }
+        int centerX = (quizLinearLayout.getLeft() + quizLinearLayout.getRight()) / 2;
+        int centerY = (quizLinearLayout.getTop() + quizLinearLayout.getBottom() / 2);
+        int radius = Math.max(quizLinearLayout.getWidth(), quizLinearLayout.getHeight());
+
+        Animator animator;
+
+        if (animateOut) {
+            animator = ViewAnimationUtils.createCircularReveal(quizLinearLayout, centerX, centerY, radius, 0);
+            animator.addListener(
+                    new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            loadNextFlag();
+                        }
+                    }
+            );
+        } else {
+            animator = ViewAnimationUtils.createCircularReveal(quizLinearLayout, centerX, centerY, 0, radius);
+        }
+        animator.setDuration(500);
+        animator.start();
+    }
+
+    private String getCountryName(String name) {
+        return name.substring(name.indexOf("-") + 1).replace('_', ' ');
+    }
+
+
 }
